e0498f517dc32a0c4a509297b69a3c9117b46ff5
CraigReeves1230 <reevescd1@gmail.com>
2018-02-02 09:35:03 -0500

    made symbolic links

--- provision.sh
+++ provision.sh
@@ -18,16 +18,22 @@ sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 
 echo "-- Installing PHP and Composer --"
-sudo apt install -y php7.0-fpm php7.0-mbstring php7.0-xml php7.0-mysql php7.0-common php7.0-gd php7.0-json php7.0-cli php7.0-curl
+sudo apt install -y php7.1-fpm php7.1-mbstring php7.1-xml php7.1-mysql php7.1-common php7.1-gd php7.1-json php7.1-cli php7.1-curl
 sudo apt-get install -y php-zip
-sudo systemctl start php7.0-fpm
+sudo systemctl start php7.1-fpm
 sudo apt-get install -y composer
 
 echo "-- Install and Start Laravel --"
 cd /vagrant
-composer install
-sudo cp nginx-default /etc/nginx/sites-available/default
-sudo cp nginx-config /etc/nginx/nginx.conf
+
+# composer install
+wget https://getcomposer.org/download/1.6.3/composer.phar
+sudo mv composer.phar /usr/local/bin/composer
+
+sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/nginx.conf
+sudo ln -s /vagrant/nginx-default /etc/nginx/sites-available/default
+sudo ln -s /vagrant/nginx-config /etc/nginx/nginx.conf
 sudo service nginx restart
 sudo service nginx reload
 sudo cp preset-env .env
